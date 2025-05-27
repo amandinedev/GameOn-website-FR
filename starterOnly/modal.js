93bbe9f6e67720720fb00e3d984e88128debac86
amandinedev <amandinehazard@live.fr>
2025-05-27 10:50:02 +0200

    feature updated launchModal function

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,9 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const indexTopnav = document.querySelector(".topnav");
+const indexHeroSection = document.querySelector(".hero-section");
+const indexFooter = document.querySelector("footer");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -18,6 +21,13 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+  // turn off index content when modal launch
+  indexHeroSection.style.display = 'none';
+  indexFooter.style.display = 'none';
+  // turn off Topnav only on larger screens
+  if (modalbg.offsetWidth >= 768){
+  indexTopnav.style.display = 'none';
+  }
 }
 
 

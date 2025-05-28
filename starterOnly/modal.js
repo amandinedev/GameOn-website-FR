3978c27b0cdd1e17e21c9c2e8fa75abf5ddff317
amandinedev <amandinehazard@live.fr>
2025-05-28 07:57:13 +0200

    feature-confirmation-form-sent

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const submitBtn = document.querySelector(".btn-submit");
+const closeBtn = document.querySelector(".btn-close-signup");
 const modalContent = document.querySelector(".content");
 const modalContentValidation = document.querySelector(".content-validation");
 const formData = document.querySelectorAll(".formData");
@@ -23,12 +24,32 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
-//close modal event
-submitBtn.addEventListener("click", validateModal);
+// //launch modal form validation
+// submitBtn.addEventListener("click", validateModal);
 
-// validate modal event
+//function to validate the form
+function validate() {
+  //validation rules
+  return true;
+}
+
+// launch validate layout
 function validateModal() {
   modalContent.style.display = "none";
   modalContentValidation.style.display = "block";
 }
 
+// Add event listener for form submit
+document.addEventListener('submit', (event) =>{
+    event.preventDefault();
+    if (validate()) {  // Call your actual validation function here
+        validateModal();  // Show success message
+    }
+});
+
+
+// close modal form
+closeBtn.addEventListener("click", closeModal);
+function closeModal() {
+  modalContentValidation.style.display = "none";
+}

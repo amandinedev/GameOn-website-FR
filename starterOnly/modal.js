d5921ec9cfdd6091d43c229b55307f20a61985b5
amandinedev <amandinehazard@live.fr>
2025-05-31 08:43:35 +0200

    feature-confirmation-form-sent

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,10 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const submitBtn = document.querySelector(".btn-submit");
+const closeBtn = document.querySelector(".btn-close-signup");
+const modalContent = document.querySelector(".content");
+const modalContentValidation = document.querySelector(".content-validation");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -18,6 +22,33 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+  modalContent.style.display = "block";
 }
 
+//function to validate the form
+function validate() {
+  //validation rules
+  return true;
+}
+
+// launch validate layout
+function validateModal() {
+  modalContent.style.display = "none";
+  modalContentValidation.style.display = "block";
+}
+
+// Add event listener for form submit
+document.addEventListener('submit', (event) =>{
+    event.preventDefault();
+    if (validate()) {  // Call validation function 
+        validateModal();  // Show success message
+    }
+});
+
+// close modal form
+closeBtn.addEventListener("click", closeModal);
+function closeModal() {
+  modalContentValidation.style.display = "none";
+  modalbg.style.display = "none";
+}
 

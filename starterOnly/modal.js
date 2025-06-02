7fc5fc36a9ad9b42a338d66fb9b0613acfb7dbff
amandinedev <amandinehazard@live.fr>
2025-06-02 10:01:29 +0200

    feature-validation-or-error-message

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -19,6 +19,7 @@ const indexTopnav = document.querySelector(".topnav");
 const indexHeroSection = document.querySelector(".hero-section");
 const indexFooter = document.querySelector("footer");
 
+/********* LAUNCH MODAL *************/
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -35,18 +36,161 @@ function launchModal() {
   }
 }
 
-//function to validate the form
-function validate() {
-  //validation rules
+/********* VALIDATION MODAL *************/
+// show error message on invalid input
+function showErrorMessage(element, message) {
+  // add attribute to switch to error style
+element.parentElement.setAttribute("data-error", message)
+element.parentElement.setAttribute("data-error-visible", "true")
+}
+// clear error message on input change
+function clearError(element) {
+  if (element.parentElement.hasAttribute('data-error')){
+  element.parentElement.removeAttribute("data-error");
+  element.parentElement.removeAttribute("data-error-visible");
+  }
+}
+// validate firstname
+function validateFirstName(){
+  // get firstname entry
+  const firstName = document.getElementById("first");
+  // Clear any previous error messages
+  clearError(firstName);
+  // First name validation
+  if (firstName.value.trim().length<2){
+  showErrorMessage(firstName, "Le prénom doit contenir au moins deux caractères");
+  return false;
+  }
   return true;
 }
+// validate lastname
+function validateLastName(){
+  // get lastname entry
+  const lastName = document.getElementById("last");
+  // Clear any previous error messages
+  clearError(lastName);
+  // First name validation
+  if (lastName.value.trim().length<2){
+  showErrorMessage(lastName, "Le nom doit contenir au moins deux caractères");
+  return false;
+  }
+  return true;
+  }
+// validate email
+function validateEmail(){
+// get email entry
+const email = document.getElementById("email");
+const emailRegex = /^[a-z0-9._-]+@[a-z0-9._-]+\.[a-z0-9._-]+$/;
+// Clear any previous error messages
+clearError(email);
+// email validation
+if (!emailRegex.test(email.value)){
+showErrorMessage(email, "Veuillez entrer une adresse email valide");
+return false;
+}
+return true;
+}
+//check age minimum
+function validateAge(){
+let birthdate = document.getElementById("birthdate");
+let birthdateValue = Date.parse(birthdate.value.trim()); 
+  // Get current time in milliseconds (UTC)
+  let currentDateMs = Date.now();
+  // Get birthdate in UTC milliseconds
+  // let birthdateMs = birthdate.getTime();
+  // Calculate the number of milliseconds in a year
+  const msPerYear = 1000*60*60*24*365.25; // accounts for leap years
+  const ageMinYears = 13;
+  const ageMinMs = ageMinYears * msPerYear;
+  console.log(ageMinMs);
+  // Calculate age in milliseconds
+  let diffMs = currentDateMs - birthdateValue;
+  console.log(diffMs);
+  // Calculate the minimum age in milliseconds
+  if (diffMs<ageMinMs){
+  return false;
+  }
+  return true;
+  }
+// validate birthdate
+function validateBirthdate(){
+  let birthdate = document.getElementById("birthdate");
+  let birthdateValue = Date.parse(birthdate.value.trim());
+  // Clear any previous error messages
+  clearError(birthdate);
+  // birthdate validation
+  if (isNaN (birthdateValue)){
+   showErrorMessage(birthdate, "Veuillez entrer une date de naissance valide.");
+   return false;
+  }
+  if (!validateAge()){
+   showErrorMessage(birthdate, "Vous devez avoir plus de 13 ans.")
+   return false;
+  }
+  return true;
+}
+// validate tournament participations
+function validateQuantity(){
+  // get quantity input
+  const quantity = document.getElementById("quantity");
+  // Clear any previous error messages
+  clearError(quantity);
+  // quantity validation
+  if (!quantity.value.match(/^[0-9]+$/)){
+  showErrorMessage(quantity, "Veuillez entrer un chiffre entre 0 et 99");
+  return false;
+}
+return true;
+}
+// validate location
+function validateLocation(){
+  const location = document.getElementById("location1");
+  // get all location radios checked
+  const locationChecked = document.querySelectorAll('input[name="location"]:checked');
+  // Clear any previous error messages
+  clearError(location);
+  // if no radio is checked, show error message
+  if (locationChecked.length === 0){
+      showErrorMessage(location, "Veuillez sélectionner une ville.");
+      return false;
+    };
+return true;      
+};
+// validate Terms
+function validateTerms(){
+  const termsCheckbox = document.getElementById('checkbox1');
+  // Clear any previous error messages
+  clearError(termsCheckbox);
+  // if no checkbox is checked, show error message
+  if (termsCheckbox.checked === false){
+  showErrorMessage (termsCheckbox, "Veuillez accepter les conditions d'utilisation.");
+  return false;
+  };
+return true;      
+};
+//function to validate the form
+function validate() {
+  let firsNameValid=validateFirstName();
+  let lastNameValid=validateLastName();
+  let emailValid=validateEmail();
+  let birthdateValid=validateBirthdate();
+  let quantityValid=validateQuantity();
+  let locationValid=validateLocation();
+  let termsValid=validateTerms();
+  //validation rules
+  if (firsNameValid && lastNameValid && emailValid && birthdateValid && quantityValid && locationValid && termsValid) {
+  return true;
+  }
+}
 
+/********* LAUNCH MODAL VALIDATED MESSAGE*************/
 // launch validate layout
 function validateModal() {
   modalContent.style.display = "none";
   modalContentValidation.style.display = "block";
 }
 
+/********* SUBMIT EVENT LISTENER *************/
 // Add event listener for form submit
 document.addEventListener('submit', (event) =>{
     event.preventDefault();
@@ -55,6 +199,7 @@ document.addEventListener('submit', (event) =>{
     }
 });
 
+/********* CLOSE MODAL *************/
 // close modal form
 closeBtn.addEventListener("click", closeModal);
 function closeModal() {

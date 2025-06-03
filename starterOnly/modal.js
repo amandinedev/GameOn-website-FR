1f357c7b2ff0bed40cc2a4b4b6f24b26b6e92cc2
amandinedev <amandinehazard@live.fr>
2025-06-03 13:39:48 +0200

    fix responsive -bg full white from width >=768

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,7 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-const indexTopnav = document.querySelector(".topnav");
+const indexTopnav = document.getElementById("myTopnav");
 const indexHeroSection = document.querySelector(".hero-section");
 const indexFooter = document.querySelector("footer");
 
@@ -22,11 +22,11 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
   // turn off index content when modal launch
-  indexHeroSection.style.display = 'none';
-  indexFooter.style.display = 'none';
+  indexHeroSection.style.display = "none"
+  indexFooter.style.display = "none";
   // turn off Topnav only on larger screens
-  if (modalbg.offsetWidth >= 768){
-  indexTopnav.style.display = 'none';
+  if (window.innerWidth >= 768){
+  indexTopnav.style.display = "none";
   }
 }
 
